537e92f4d30b2cdad9b0dd275b4dabd7c17de902
cilim <os.chilim@gmail.com>
2015-01-22 12:18:46 +0100

    User with password

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe "User" do
+  describe "passwords" do
+    it "needs a password to be valid" do
+      u = User.new(name: "Marko")
+      u.save
+      expect(u).to_not be_valid
+
+      u.password = "password"
+      u.password_confirmation = "password2"
+      u.save
+      expect(u).to_not be_valid
+
+      u.password_confirmation = "password"
+      u.save
+      expect(u).to be_valid
+    end
+  end
+
+  describe "authentication" do
+    let(:user) { User.create(name: "Marko", password: "password", password_confirmation: "password") }
+
+    it "authenticates with correct password" do
+      expect(user.authenticate("password")).to be
+    end
+
+    it "doesnt authenticates with an incorrect password" do
+      expect(user.authenticate("password2")).to_not be
+    end
+  end
+end
